531cf515ab3381b5b1e1e943fe1a9f925dd48834
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-18 11:07:22 -0700

    Add formatSpots function for displaying the appropriate message based on props.spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,14 +9,22 @@ export default function DayListItem(props) {
     "day-list__item--full": props.spots === 0
   });
 
+  const formatSpots = function() {
+    if (props.spots === 0) {
+      return 'no spots remaining';
+    } else if (props.spots === 1) {
+      return '1 spot remaining';
+    } else {
+      return `${props.spots} spots remaining`;
+    }
+  };
+
   return (
     <li className={listClass} onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">Day Name</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
 
-// day-list__item all the time
-// day-list__item--selected class name if props.selected is true
-// day-list__item--full class name if props.spots is 0.
+
