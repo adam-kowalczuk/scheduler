7453500c189f0d33f4974d26c6a2da02f4d1914a
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-19 15:11:38 -0700

    Invoke states for student and interviewer, include properties to make input a controlled component and <InterviewerList> a controlled list

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,6 +4,9 @@ import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -13,20 +16,20 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-          /*
-            This must be a controlled component
-            your code goes here
-          */
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
           />
         </form>
         <InterviewerList
-        /* your code goes here */
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger >Cancel</Button>
-          <Button confirm >Save</Button> 
+          <Button confirm >Save</Button>
         </section>
       </section>
     </main>
