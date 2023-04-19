56bf4ffd0c48f5c2884a7cf8a08298dd7b90daa8
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-18 20:33:36 -0700

    Refactor 'Clickable' InterviewListItem story to pass down interviewer.id as an argument of setInterviewer function

--- stories/index.js
+++ stories/index.js
@@ -101,10 +101,9 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
       ))
       .add("Clickable", () => (
         <InterviewerListItem
-          id={interviewer.id}
           name={interviewer.name}
           avatar={interviewer.avatar}
-          setInterviewer={action("setInterviewer")}
+          setInterviewer={action("setInterviewer")(interviewer.id)}
         />
       ));
 
