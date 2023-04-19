8ee4173e6214450de4b1f520a8181af28baf4cf3
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-18 17:39:53 -0700

    Import InterviewerListItem component and create listInterviewers array which renders each interviewer

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,27 @@
 import React from "react";
 
-import "components/InterviewerList.scss"
+import "components/InterviewerList.scss";
+
+import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
+
+  const interviewers = props.interviewers;
+  const listInterviewers = interviewers.map((interviewer) =>
+    <InterviewerListItem
+      key={interviewer.id}
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer={props.setInterviewer}
+      selected={interviewer.id === props.interviewer}
+    />
+  );
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{listInterviewers}</ul>
     </section>
   );
 }
