b28a79b31af5b7628d1650625a20deaed702e6b3
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-20 20:11:57 -0700

    Add get request to api/interviewers and set interviewers state

--- src/components/Application.js
+++ src/components/Application.js
@@ -13,20 +13,23 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
-  
+
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const setDay = day => setState({ ...state, day });
 
   useEffect(() => {
     Promise.all([
       axios.get('api/days'),
-      axios.get('api/appointments')
+      axios.get('api/appointments'),
+      axios.get('api/interviewers')
     ]).then((all) => {
       console.log("Days: ", all[0]); // first
       console.log("Appointments: ", all[1]); // second
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }));
+      console.log("Interviewers: ", all[2]);
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
   }, []);
 
