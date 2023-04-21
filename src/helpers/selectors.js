ba8ac21a0451468327efae32c1a1fd8a58c7c19e
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-21 09:49:05 -0700

    Create getInterview selector function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,27 +1,50 @@
-export default function getAppointmentsForDay(state, day) {
+export function getAppointmentsForDay(state, day) {
   //Days array
   const days = state.days;
 
   //Check if days array is empty 
   if (!days.length) {
-    return days;
+    return [];
   }
 
   //Appointments array
   const appointments = Object.values(state.appointments);
 
   //Create an array of day's scheduled appointment ids 
-  let dayApps = [];
-  for (const dayObj of days) {
-    if (dayObj.name === day) {
-      dayApps = dayObj.appointments;
+  let dayAppointments = [];
+  for (const d of days) {
+    if (d.name === day) {
+      dayAppointments = [...d.appointments];
       break;
     }
   }
 
   //Create an array with all of day's scheduled appointments
-  const appsArray = appointments.filter((app) => dayApps.includes(app.id));
+  const selectedAppointments = appointments.filter(appointment => dayAppointments.includes(appointment.id));
 
-  return appsArray;
+  return selectedAppointments;
 }
+
+export function getInterview(state, interview) {
+  //If no interview exists, return null
+  if (!interview) {
+    return null;
+  }
+
+  //Create an array of interviewer objects
+  const interviewers = Object.values(state.interviewers);
+  let revisedInterview;
+
+  //Loop through interviewers array to find a match with interviewer id from interview, then replace id with interviewer object
+  for (const interviewer of interviewers) {
+    if (interview.interviewer === interviewer.id) {
+      revisedInterview = { ...interview, interviewer: interviewer };
+    }
+  }
+
+  return revisedInterview;
+}
+
+
+
 
