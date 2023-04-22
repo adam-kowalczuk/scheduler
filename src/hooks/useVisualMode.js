55e219766c6ae1827a8f3f462ee24d583a70b480
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-22 09:16:39 -0700

    Clean up comments and superfluous code

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,18 +1,15 @@
 import { useState } from 'react';
 
 export default function useVisualMode(initialMode) {
-  const [mode, setMode] = useState(initialMode);
   const [history, setHistory] = useState([initialMode]);
 
   //Set new mode and add to history
   const transition = function(newMode, replace = false) {
+    //If replace is truthy, remove last element in history and add newMode. Otherwise, just add newMode
     if (replace) {
-      console.log('History BEFORE Replace', history)
       setHistory((prev) => [...prev.slice(0, prev.length - 1), newMode]);
-      console.log('History AFTER Replace', history);
     } else {
       setHistory((prev) => [...prev, newMode]);
-      console.log('History WITHOUT Replace', history)
     }
   };
 
@@ -23,26 +20,10 @@ export default function useVisualMode(initialMode) {
     }
   };
 
-
-  //   if (replace) {
-  //     setMode(newMode);
-  //   } else {
-  //     setMode(newMode);
-  //     setHistory([...history, newMode]);
-  //   }
-  // };
-
-  // //Go back to previous mode
-  // const back = function() {
-  //   if (history.length > 1) {
-  //     history.pop();
-  //     setMode(history[history.length - 1]);
-  //     setHistory([...history]);
-  //   }
-
   return {
     mode: history[history.length - 1],
     transition,
     back
   };
 };
+
