b96b3d0ee773739b206b58564e374f53699e6192
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-22 09:45:10 -0700

    Remove unnecessary console logs

--- src/components/Application.js
+++ src/components/Application.js
@@ -26,9 +26,6 @@ export default function Application(props) {
       axios.get('api/appointments'),
       axios.get('api/interviewers')
     ]).then((all) => {
-      console.log("Days: ", all[0]); // first
-      console.log("Appointments: ", all[1]); // second
-      console.log("Interviewers: ", all[2]);
       setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
   }, []);
