f63c962962f4570eeba10ce2591fa1d149ede2a5
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-21 17:45:46 -0700

    Add logic for replacing mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,21 +4,45 @@ export default function useVisualMode(initialMode) {
   const [mode, setMode] = useState(initialMode);
   const [history, setHistory] = useState([initialMode]);
 
-  const transition = function(newMode) {
-    setMode(newMode);
-    setHistory([...history, newMode]);
-  };
-
-  const back = function() {
-    if (history.length > 1) {
-      history.pop();
-      setMode(history[history.length - 1]);
+  //Set new mode and add to history
+  const transition = function(newMode, replace = false) {
+    if (replace) {
+      console.log('History BEFORE Replace', history)
+      setHistory((prev) => [...prev.slice(0, prev.length - 1), newMode]);
+      console.log('History AFTER Replace', history);
+    } else {
+      setHistory((prev) => [...prev, newMode]);
+      console.log('History WITHOUT Replace', history)
     }
   };
 
+  //Go back to previous mode
+  const back = function() {
+    if (history.length > 1) {
+      setHistory((prev) => [...prev.slice(0, prev.length - 1)]);
+    }
+  };
+
+
+  //   if (replace) {
+  //     setMode(newMode);
+  //   } else {
+  //     setMode(newMode);
+  //     setHistory([...history, newMode]);
+  //   }
+  // };
+
+  // //Go back to previous mode
+  // const back = function() {
+  //   if (history.length > 1) {
+  //     history.pop();
+  //     setMode(history[history.length - 1]);
+  //     setHistory([...history]);
+  //   }
+
   return {
-    mode,
+    mode: history[history.length - 1],
     transition,
     back
   };
-}
+};
