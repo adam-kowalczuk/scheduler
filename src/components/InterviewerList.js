55459b48ee227ed867dfe6fb8fedae3869732d21
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-24 18:24:30 -0700

    Change props.value to props.value.id

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -7,15 +7,16 @@ import InterviewerListItem from "./InterviewerListItem";
 export default function InterviewerList(props) {
 
   const interviewers = props.interviewers;
-  const listInterviewers = interviewers.map((interviewer) =>
-    <InterviewerListItem
+  const listInterviewers = interviewers.map((interviewer) => {
+
+    return <InterviewerListItem
       key={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      selected={interviewer.id === props.value}
+      selected={interviewer.id === props.value.id}
       setInterviewer={() => props.onChange(interviewer.id)}
-    />
-  );
+    />;
+  });
 
   return (
     <section className="interviewers">
