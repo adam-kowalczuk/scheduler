46e66ed32be9758c7f5f373c87f9d69ee2b8960d
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-24 19:37:48 -0700

    Add ERROR_SAVE and ERROR_DELETE modes, edit transitions to replace rather that simply add to history, change props.interview.interviewer to props.interview.interviewer.id

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -8,6 +8,7 @@ import Empty from "./Empty";
 import Form from "./Form";
 import Status from "./Status";
 import Confirm from "./Confirm";
+import Error from "./Error";
 
 import useVisualMode from "hooks/useVisualMode";
 
@@ -19,6 +20,8 @@ export default function Appointment(props) {
   const DELETING = "DELETING";
   const CONFIRM = "CONFIRM";
   const EDIT = "EDIT";
+  const ERROR_SAVE = "ERROR_SAVE";
+  const ERROR_DELETE = "ERROR_DELETE";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -33,20 +36,19 @@ export default function Appointment(props) {
 
     transition(SAVING);
 
-    props.bookInterview(props.id, interview)
-      .then(() => {
-        transition(SHOW);
-      });
+    props
+      .bookInterview(props.id, interview)
+      .then(() => transition(SHOW))
+      .catch((error) => transition(ERROR_SAVE, true));
   };
 
   //Transition to Status component, delete appointment, then transition to Empty component
-  const cancel = function() {
-    transition(DELETING);
+  const destroy = function() {
+    transition(DELETING, true);
 
     props.cancelInterview(props.id)
-      .then(() => {
-        transition(EMPTY);
-      });
+      .then(() => transition(EMPTY))
+      .catch((error) => transition(ERROR_DELETE, true));
   };
 
   //Show Confirm component before deleting appointment
@@ -76,16 +78,18 @@ export default function Appointment(props) {
       )}
       {mode === SAVING && <Status message="Saving" />}
       {mode === DELETING && <Status message="Deleting" />}
-      {mode === CONFIRM && <Confirm message="Are you sure you want to delete?" onCancel={() => back(SHOW)} onConfirm={cancel} />}
+      {mode === CONFIRM && <Confirm message="Are you sure you want to delete?" onCancel={() => back(SHOW)} onConfirm={destroy} />}
       {mode === EDIT && (
         <Form
           student={props.interview.student}
-          interviewer={props.interview.interviewer}
+          interviewer={props.interview.interviewer.id}
           interviewers={props.interviewers}
           onCancel={() => back(EMPTY)}
           onSave={save}
         />
       )}
+      {mode === ERROR_SAVE && <Error onClose={() => back(CREATE)} />}
+      {mode === ERROR_DELETE && <Error onClose={() => back(SHOW)} />}
     </article>
   );
 }
