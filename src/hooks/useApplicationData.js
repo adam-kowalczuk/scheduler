7c3f9f33f3a03f8c6a0ca7b138037098a441ec46
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-24 22:03:24 -0700

    Refactor logic used for decreasing number of appointment spots left to increase when an appointment is deleted

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function useApplicationData() {
-  //State of scheduler
+  //Set default state
   const [state, setState] = useState({
     day: "Monday",
     days: [],
@@ -10,7 +10,7 @@ export default function useApplicationData() {
     interviewers: {},
   });
 
-  //Send multiple GET requests to database on initial load for days, appointments and interviewers and update state
+  //Populate app with data from database
   useEffect(() => {
     Promise.all([
       axios.get("api/days"),
@@ -26,10 +26,10 @@ export default function useApplicationData() {
     });
   }, []);
 
-  //Set appointment day 
+  //Sets current day
   const setDay = (day) => setState({ ...state, day });
 
-  //Book an interview and store in database
+  //Book an interview, store it in database, and decrease the number of open appointment spots for the day
   const bookInterview = function(id, interview) {
     const appointment = {
       ...state.appointments[id],
@@ -42,7 +42,7 @@ export default function useApplicationData() {
 
     //Find the day where the appointment is booked
     const foundDay = state.days.find((day) => day.appointments.includes(id));
-    //Reduce the number of open appointment spots by 1 if appointment is successfully added
+    //Decrease the number of open appointment spots by 1 if appointment is successfully added
     const days = state.days.map((day) => {
       if (day.name === foundDay.name && state.appointments[id].interview === null) {
         return { ...day, spots: day.spots - 1 };
@@ -57,21 +57,31 @@ export default function useApplicationData() {
       });
   };
 
-  //Cancel an interview and remove from database
+  //Cancel an interview, remove it from database, and increase the number of open appointment spots for the day
   const cancelInterview = function(id) {
     const appointment = {
       ...state.appointments[id],
       interview: null
     };
-
     const appointments = {
       ...state.appointments,
       [id]: appointment
     };
 
+    //Find the day where the appointment is booked
+    const foundDay = state.days.find((day) => day.appointments.includes(id));
+    //Increase the number of open appointment spots by 1 if appointment is successfully deleted
+    const days = state.days.map((day) => {
+      if (day.name === foundDay.name && state.appointments[id].interview !== null) {
+        return { ...day, spots: day.spots + 1 };
+      } else {
+        return day;
+      }
+    });
+
     return axios.delete(`api/appointments/${id}`)
       .then(() => {
-        setState({ ...state, appointments: appointments });
+        setState({ ...state, appointments, days });
       });
   };
 
