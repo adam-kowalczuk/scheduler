945f9a5f293a3ffc54808e61d40016df2390fba8
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-24 17:10:13 -0700

    Create cancelInterview function and pass it down to the Appointments component

--- src/components/Application.js
+++ src/components/Application.js
@@ -40,6 +40,24 @@ export default function Application(props) {
       });
   };
 
+  const cancelInterview = function(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return axios.delete(`api/appointments/${id}`)
+      .then(() => {
+        setState({ ...state, appointments: appointments });
+      });
+  };
+
+
   useEffect(() => {
     Promise.all([
       axios.get("api/days"),
@@ -67,6 +85,7 @@ export default function Application(props) {
         interview={interview}
         interviewers={interviewers}
         bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
       />
     );
   });
