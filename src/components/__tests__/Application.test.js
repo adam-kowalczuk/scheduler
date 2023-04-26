97632248fb2e5fb0d86a76d1f69dd292dd6cdb0b
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-26 14:04:12 -0700

    Add expect statment for testing whether appointment is saving

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -27,7 +27,7 @@ describe("Application", () => {
   });
 
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-    const { container } = render(<Application />);
+    const { container, debug } = render(<Application />);
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
@@ -43,7 +43,11 @@ describe("Application", () => {
 
     fireEvent.click(getByText(appointment, "Save"));
 
-    console.log(prettyDOM(appointment));
+    debug();
+
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    // console.log(prettyDOM(appointment));
   });
 })
 
