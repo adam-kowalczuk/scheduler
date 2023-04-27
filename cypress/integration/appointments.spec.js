a7e87f3fda67c7091e51fa3f373d210451775040
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-27 13:00:56 -0700

    Finish test for deleting an appointment

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -44,17 +44,10 @@ describe('Appointments', () => {
       .first()
       .click({ force: true });
 
-    // cy.get("[data-testid=student-name-input]")
-    //   .clear()
-    //   .type("Lydia Miller-Jones");
-
-    // cy.get("[alt='Tori Malcolm']").click();
-
     cy.contains("Confirm").click();
     cy.contains("Deleting").should('exist');
     cy.contains("Deleting").should('not.exist');
 
-    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-    cy.contains(".appointment__card--show", "Tori Malcolm");
+    cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
   });
 });
